b0ec029100914949f975d569e14c4e5cd94dfb32
Artem <artmart95@mail.ru>
2020-06-07 10:33:04 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+"use strict"
+
+// const result = confirm("Ты тут?");
+// console.log(result);
+
+// const answer = prompt("Вам есть 18?", "18");
+// console.log(answer);
+
+const answers = [];
+
+// answers[0] = prompt('Как вас зовут?', "");
+// answers[1] = prompt('Как ваша фамилия?', "");
+// answers[2] = +prompt('Сколько вам лет?', "");
+
+console.log(typeof(answers));
+
+let incr = 10,
+    decr = 10;
+
+// ++incr; 
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
